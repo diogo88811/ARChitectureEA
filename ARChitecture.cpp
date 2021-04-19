25ad9610c1f4054b16a46a78ea446b55b718086e
diogo <diogovalente00@gmail.com>
2021-04-19 20:21:41 +0100

    140 run time error

--- ARChitecture.cpp
+++ ARChitecture.cpp
@@ -10,54 +10,94 @@ h ≤ 500
 H ≤ 60000
 */
 
-//function to debug 
-void print(int n, int h, int H){
-  cout << "n -> " << n << endl;
-  cout << "h -> " << h << endl;
-  cout << "H -> " << H << endl;
-}
-
-//function to debug 
-void printRoom(int n,int H,vector<vector<int>> room){
-  for(int i = 0 ; i < H ; i++){
-    for(int j = 0 ; j < n ; j ++){
-      cout << room[i][j] << " ";
-    }
-    cout << endl;
-  }
-}
-
-int mod_abs(int a, int mod) {
+int mod_abs(int a, int mod)
+{
   return ((a % mod) + mod) % mod;
 }
 
-int mod_add(int a, int b, int mod) {
+int mod_add(int a, int b, int mod)
+{
   return (mod_abs(a, mod) + mod_abs(b, mod)) % mod;
 }
 
-int mod_sub(int a, int b, int mod) {
+int mod_sub(int a, int b, int mod)
+{
   return mod_add(a, -b, mod);
 }
 
-void play(int n, int h, int H){
-  vector<vector<int>> room(H, vector<int>(n));
-  printRoom(n,H,room);
+
+void play(int n, int h, int H)
+{
+  int room[H][n][2];
+
+  int solution = 0;
+
+  for (int i = 0; i < H; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      room[i][j][0] = 0;
+      room[i][j][1] = 0;
+    }
+  }
+
+  //base case
+  room[H - h][0][0] = 1;
+
+  for (int i = 0; i < n - 1; i++)
+  {
+    for (int j = 0; j < H; j++)
+    {
+      if (room[j][i][0] >= 1)
+      { // going up
+        for (int k = 0; k < h - 1; k++)
+        {
+          
+          room[j - k - 1][i + 1][0] = mod_add(room[j - k - 1][i + 1][0],room[j][i][0],1000000007);
+        }
+
+        for (int k = 0; k < h-1; k++)
+        {
+          if((j + k + 1) < H-h+1){
+            
+            room[j + k + 1][i + 1][1] = mod_add(room[j + k + 1][i + 1][1],room[j][i][0],1000000007);
+          }
+        }
+      }
+      if (room[j][i][1] >= 1)
+      { //going down
+        for (int k = 0; k < h-1; k++)
+        {
+          if((j + k + 1) < H-h+1){
+            room[j + k + 1][i + 1][1] = mod_add(room[j + k + 1][i + 1][1],room[j][i][1],1000000007);
+          }
+          
+        }
+      }
+    }
+  }
+
+  for(int i = 0; i < n; i++){
+    solution = mod_add(room[H-h][i][1], solution,1000000007);
+  }
+
+  cout << mod_abs(solution,1000000007) << endl;
+
 }
 
 int main()
 {
-    int ntestCases, n, h, H;
+  int ntestCases, n, h, H;
 
-    cin >> ntestCases;
+  cin >> ntestCases;
 
-    for (int i = 0; i < ntestCases; i++)
-    {
-      cin >> n;
-      cin >> h;
-      cin >> H;
+  for (int i = 0; i < ntestCases; i++)
+  {
+    cin >> n;
+    cin >> h;
+    cin >> H;
 
-      play(n,h,H);
+    play(n, h, H);
 
-    }
-    
+  }
 }
